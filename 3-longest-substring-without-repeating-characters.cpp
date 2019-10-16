82762a835eb90402e6487c6ddad6b493bea179a3
liph <3138519115@qq.com>
2019-10-16 20:40:36 +0800

    10-16

--- 3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters.cpp
@@ -27,9 +27,9 @@ public:
         
     }
 };
-/*
-Given a string, find the length of the longest substring without repeating characters.
-*/
+
+/* 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。*/
+
 int main()
 {
     string str("abcabcbb");
