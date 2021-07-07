07620ec83887584fccb7e8997ba75c8df003ead9
liph <3138519115@qq.com>
2021-07-07 22:13:48 +0800

    jz-43

--- cpp/jz-43.cpp
+++ cpp/jz-43.cpp
@@ -5,22 +5,29 @@
  */
 
 #include <iostream>
+#include <cmath>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
+    unordered_map<int, int> cache;
+    
     int countDigitOne(int n) {
+        if (cache.find(n) != cache.end())
+            return cache[n];
+        if (n == 0)
+            return cache[n] = 0;
         if (n < 10)
-            return 1;
-        if (isBase10(n)) // 10...0
-            return 1 + countDigitOne(n - 1);
-        if (n % 10 == 0) { // abc..0
-            return countOne(n) + countDigitOne(n - 1);
+            return cache[n] = 1;
+        if (isBase10(n)) { // a0...0
+            return cache[n] = countOne(getBase10(n)) + countDigitOne(n - 1);
         }
-
-        // abc..x 
-        int x = n % 10;
-        return (x + 1) * countOne(n / 10) + countDigitOne(x) + countDigitOne(n - x - 1);
+        
+        // abc..
+        int x = getBase10(n);
+        int y = n - x;
+        return cache[n] = (y + 1) * countOne(x) + countDigitOne(y) + countDigitOne(x - 1);
     }
 
 private:
@@ -32,7 +39,16 @@ private:
                 return false;
             n /= 10;
         }
-        return n == 1;
+        return true;
+    }
+
+    int getBase10(int n) {
+        int i = 0;
+        while (n >= 10) {
+            n /= 10;
+            i++;
+        }
+        return n * pow(10, i);
     }
 
     int countOne(int n) {
@@ -48,8 +64,9 @@ private:
 
 int main()
 {
-    cout << Solution().countDigitOne(12) << endl;       // 5
-    cout << Solution().countDigitOne(13) << endl;       // 6
-    cout << Solution().countDigitOne(3184191) << endl;  // 2978524
-    cout << Solution().countDigitOne(824883294) << endl;// 767944060
+    cout << Solution().countDigitOne(12) << endl;           // 5
+    cout << Solution().countDigitOne(13) << endl;           // 6
+    cout << Solution().countDigitOne(3184191) << endl;      // 2978524
+    cout << Solution().countDigitOne(824883294) << endl;    // 767944060
+    cout << Solution().countDigitOne(999999999) << endl;    // 900000000
 }
