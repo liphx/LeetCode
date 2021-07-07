d256333b85e0764694a128783de00ffbb21d621d
liph <3138519115@qq.com>
2021-07-07 21:09:38 +0800

    jz-41

--- cpp/jz-41.cpp
+++ cpp/jz-41.cpp
@@ -26,15 +26,13 @@ public:
         else
             q2.push(num);
 
-        if (!q1.empty() && !q2.empty()) {
-            if (q1.top() > q2.top()) {
-                int x = q1.top();
-                int y = q2.top();
-                q1.pop();
-                q2.pop();
-                q1.push(y);
-                q2.push(x);
-            }
+        if (!q1.empty() && !q2.empty() && q1.top() > q2.top()) {
+            int x = q1.top();
+            int y = q2.top();
+            q1.pop();
+            q2.pop();
+            q1.push(y);
+            q2.push(x);
         }
     }
     
